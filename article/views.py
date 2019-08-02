595530d6495e01fc67f550f0efe61c51f49e6da5
udipta <iamudi03@gmail.com>
2019-08-02 14:03:18 +0530

    create view and url to access app

--- article/views.py
+++ article/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Author, Article
 
 # Create your views here.
+
+
+class ArticleView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        return Response({"articles": articles})
