dccdf60cbc2573e0c42020908fe4a1e94d2f83d7
udipta <iamudi03@gmail.com>
2019-08-02 15:40:54 +0530

    add the author_id field into the serializer to create articles

--- article/views.py
+++ article/views.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -13,3 +14,20 @@ class ArticleView(APIView):
         # the many param informs the serializer that it will be serializing more than a single article.
         serializer = ArticleSerializer(articles, many=True)
         return Response({"articles": serializer.data})
+
+    def post(self, request):
+        article = request.data.get('article')
+
+        # Create an article from the above data
+        serializer = ArticleSerializer(data=article)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+
+    def put(self, request, pk):
+        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
+        data = request.data.get('article')
+        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({"success": "Article '{}' updated successfully".format(article_saved.title)})
