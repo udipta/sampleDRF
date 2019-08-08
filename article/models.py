4a89b3b4b38d233ec079ee3c419b23ccde303122
udipta <iamudi03@gmail.com>
2019-08-08 18:41:10 +0530

    add user view

--- article/models.py
+++ article/models.py
@@ -8,7 +8,7 @@ class Article(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     body = models.TextField()
-    author = models.ForeignKey('auth.User', related_name='articles', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', related_name="articles", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- article/serializers.py
+++ article/serializers.py
@@ -1,8 +1,8 @@
-'''
+"""
     Serializers allow complex data such as querysets and model instances
     to be converted to native Python datatypes that can then be easily
     rendered into JSON, XML or other content types.
-'''
+"""
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
