f91a87551c512abbcc2fb30ac6206774f17834d5
udipta <iamudi03@gmail.com>
2019-08-08 18:20:43 +0530

    snippet: basic request and response api call

--- snippet/views.py
+++ snippet/views.py
@@ -1,8 +1,7 @@
-from django.contrib.auth.models import User
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from .models import Snippet
 from .serializers import SnippetSerializer
 
